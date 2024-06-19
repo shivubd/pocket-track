a28089cfac5fb61f9c962692afd0001913322f1f
Shivaraj <shivarajdeyannavar@gmail.com>
2024-06-19 16:08:49 +0530

    Initial commit

---
+++ api/src/main/java/com/pocket/track/api/PocketTrackApplication.java
@@ -0,0 +1,13 @@
+package com.pocket.track.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketTrackApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/pocket/track/api/PocketTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocket.track.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
